84a035d19fddc524390865c93b4964e3e483fdf6
imrantvlk <imran.manzoor@traveloka.com>
2019-07-29 17:59:07 +0530

    added new files

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a comment!
-echo Hello World >> ginger.txt        # This is a comment, too!
-echo Hello notes >> notes.txt
+echo AKIAIOSFODNN7EXAMPLE >> ginger.txt        # This is a comment, too!
+echo https://hooks.slack.com/services/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 >> notes.txt
 git add .
 git commit -m "added new files"
 git push
