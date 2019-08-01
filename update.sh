c0726e0ff1f20b900a7338efe937e720be076a0c
imrantvlk <imran.manzoor@traveloka.com>
2019-08-01 16:43:33 +0530

    added new files

--- update.sh
+++ update.sh
@@ -2,6 +2,7 @@
 # This is a comment!
 echo key: AKIAIOSFODNN7EXAMPLE >> ginger.txt        # This is a comment, too!
 echo slack_token: https://hooks.slack.com/services/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 >> notes.txt
+echo key: AKIAIOSFODNN7EXAMPLE // ignoreLineGitseer >> ginger.txt   
 git add .
 git commit -m "added new files"
 git push
