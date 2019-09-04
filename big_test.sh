e77d8f9e7b49d8b005d7a074128fe2cc63c807d6
imrantvlk <imran.manzoor@traveloka.com>
2019-09-04 16:11:55 +0530

    Test: Comment feature

--- big_test.sh
+++ big_test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 for ((i=0; i < 1000; i++))
     do
-        echo key: AKIAIOSFODNN7EXAMPLE > look_here_gingee.txt
+        echo key: AKIAIOSFODNN7EXAMPLE >> look_here_gingee.txt
     done 
 git add .
 git commit -m "Test: Comment feature"
