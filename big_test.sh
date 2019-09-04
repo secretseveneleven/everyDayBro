e812ed3d17ed99dddd28c05885264c2fd1947a91
imrantvlk <imran.manzoor@traveloka.com>
2019-09-04 16:10:55 +0530

    Test: Comment feature

--- big_test.sh
+++ big_test.sh
@@ -2,15 +2,7 @@
 for ((i=0; i < 1000; i++))
     do
         echo key: AKIAIOSFODNN7EXAMPLE > look_here_gingee.txt
-    done  
-for ((i=0; i < 1000; i++))
-    do
-        echo key: AKIAIOSFODNN7EXAMPLE > look_here_gingee.txt
-    done  
-for ((i=0; i < 1000; i++))
-    do
-        echo key: AKIAIOSFODNN7EXAMPLE > another_ginger.txt
-    done  
+    done 
 git add .
 git commit -m "Test: Comment feature"
 git push
