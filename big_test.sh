e56cfce2ccbb35eccc02d9d7e4cfc9dbb0a0707a
imrantvlk <imran.manzoor@traveloka.com>
2019-09-24 18:05:10 +0530

    Test: 1000 lines

--- big_test.sh
+++ big_test.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
-for ((i=0; i < 5; i++))
+for ((j=0; j < 5; j++))
     do
         for ((i=0; i < 5; i++))
             do
                 echo key: AKIAIOSFODNN7EXAMPLE >> look_here_gingee.txt
+                # echo 'Added line'
             done 
         git add .
         git commit -m "Test: 1000 lines"
         git push
+        # echo 'Git push'
     done
