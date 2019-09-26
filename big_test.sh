19c6d819db83fa8bcf7c916231e462f455064b90
imrantvlk <imran.manzoor@traveloka.com>
2019-09-26 14:42:21 +0530

    Test: 1000 lines

--- big_test.sh
+++ big_test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 for ((j=0; j < 100; j++))
     do
-        for ((i=0; i < 1000; i++))
+        for ((i=0; i < 5; i++))
             do
                 echo key: AKIAIOSFODNN7EXAMPLE >> look_here_gingee.txt
                 # echo 'Added line'
